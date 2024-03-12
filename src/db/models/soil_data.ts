ce63c05b28c74632c0eeabd473ce229ebcf34b98
zackmorgs <4538979+zackmorgs@users.noreply.github.com>
2024-03-12 13:21:50 -0500

    updated EcowittDataSchema

--- src/db/models/soil_data.ts
+++ src/db/models/soil_data.ts
@@ -1,204 +1,149 @@
-import mongoose, { mongo } from 'mongoose';
+  import mongoose, { mongo } from 'mongoose';
 
-const BaseSoilSchema = new mongoose.Schema(
-    {
-        timestamp: Date,
-        metadata: {
-            data_type: String,
-            deviceMAC: String,
-            location: {
-                lat: Number,
-                lon: Number,
-                altitude: { type: Number, required: false }
-            }
-        },
-    },
-    {
-        timeseries: {
-            timeField: 'timestamp',
-            metaField: 'metadata',
-            granularity: 'hours',
-        }
-    }
-);
+  const BaseSoilSchema = new mongoose.Schema(
+      {
+          timestamp: Date,
+          metadata: {
+              data_type: String,
+              deviceMAC: String,
+              location: {
+                  lat: Number,
+                  lon: Number,
+                  altitude: { type: Number, required: false }
+              }
+          },
+      },
+      {
+          timeseries: {
+              timeField: 'timestamp',
+              metaField: 'metadata',
+              granularity: 'hours',
+          }
+      }
+  );
 
-export interface BaseSoilData extends mongoose.Document {
-    timestamp: Date;
-    metadata: {
-        type: string;
-        deviceMAC: string;
-        location: {
-            lat: number;
-            lon: number;
-            altitude?: number
-        };
-    };
+  export interface BaseSoilData extends mongoose.Document {
+      timestamp: Date;
+      metadata: {
+          type: string;
+          deviceMAC: string;
+          location: {
+              lat: number;
+              lon: number;
+              altitude?: number
+          };
+      };
 
-}
+  }
 
-export const BaseSoilModel = mongoose.model<BaseSoilData>('soil', BaseSoilSchema);
+  export const BaseSoilModel = mongoose.model<BaseSoilData>('soil', BaseSoilSchema);
 
-const AmbientDataSchema = new mongoose.Schema({
+  const AmbientDataSchema = new mongoose.Schema({
 
-});
+  });
 
-export const AmbientDataModel = BaseSoilModel.discriminator('ambient_data', AmbientDataSchema);
+  export const AmbientDataModel = BaseSoilModel.discriminator('ambient_data', AmbientDataSchema);
 
-export interface AmbientData extends BaseSoilData {
-}
+  export interface AmbientData extends BaseSoilData {
+  }
 
-const EcowittDataSchema = new mongoose.Schema({
-    soiltemp1f: {
-
-    },
-    soiltemp2f: {
-
-    },
-    soiltemp3f: {
-
-    },
-    soiltemp4f: {
-
-    },
-    soiltemp5f: {
-
-    },
-    soiltemp6f: {
-
-    },
-    soiltemp7f: {
-
-    },
-    soiltemp9f: {
-
-    },
-    soiltemp10f: {
-
-    },
-    soilhum1: {
-
-    },
-    soilhum2: {
-
-    },
-    soilhum3: {
-
-    },
-    soilhum4: {
-
-    },
-    soilhum5: {
-
-    },
-    soilhum6: {
-
-    },
-    soilhum7: {
-
-    },
-    soilhum8: {
-
-    },
-    soilhum9: {
-
-    },
-    soilhum10: {
-
-    },
-    soiltens1: {
-
-    },
-    soiltens2: {
-
-    },
-    soilten3: {
-
-    },
-    soilten4: {
-    },
-    battsm1 : {
-
-    },
-    battsm2: {
-
-    },
-    battsm3: {
-
-    },
-    battsm4: {
-        
-    }
-
-});
-
-export const EcowittDataModel = BaseSoilModel.discriminator('ecowitt_data', EcowittDataSchema);
-
-export interface EcowittData extends BaseSoilData {
+  const EcowittDataSchema = new mongoose.Schema({
     soil_ch1: {
-        soilmoisture: {
-          time: number,
-          unit: "%",
-          value: number
-        }
-      },
-      soil_ch2: {
-        soilmoisture: {
-          time: number,
-          unit: "%",
-          value: number
-        }
-      },
-      soil_ch3: {
-        soilmoisture: {
-          time: number,
-          unit: "%",
-          value: number
-        }
-      },
-      soil_ch4: {
-        soilmoisture: {
-          time: number,
-          unit: "%",
-          value: number
-        }
-      },
-      soil_ch5: {
-        soilmoisture: {
-          time: number,
-          unit: "%",
-          value: number
-        }
-      },
-      soil_ch6: {
-        soilmoisture: {
-          time: number,
-          unit: "%",
-          value: number
-        }
-      },
-      soil_ch7: {
-        soilmoisture: {
-          time: number,
-          unit: "%",
-          value: number
-        }
-      },
-      soil_ch8: {
-        "soilmoisture": {
-          time: number,
-          unit: "%",
-          value: number
-        }
-      },
-}
+      soilmoisture: {
+        time: {type: Number, required: false},
+        unit: {type: String, required: false},
+        value: {type: Number, required: false}
+      }
+    },
+    soil_ch2: {
+      soilmoisture: {
+        time: {type:Number, required: false},
+        unit: {type:String, required: false},
+        value: {type:Number, required: false}
+      }
+    },
+    soil_ch3: {
+      soilmoisture: {
+        time: {type:Number, required: false},
+        unit: {type:String, required: false},
+        value: {type:Number, required: false}
+      }
+    },
+    soil_ch4: {
+      soilmoisture: {
+        time: {type:Number, required:false},
+        unit: {type:String, required: false},
+        value: {type:Number, required:false}
+      }
+    },
+    soil_ch5: {
+      soilmoisture: {
+        time: {type:Number, required:false},
+        unit: {type:String, required: false},
+        value: {type:Number, required:false}
+      }
+    },
+    soil_ch6: {
+      soilmoisture: {
+        time: {type:Number, required:false},
+        unit: {type:String, required: false},
+        value: {type:Number, required:false}
+      }
+    },
+    soil_ch7: {
+      soilmoisture: {
+        time: {type:Number, required:false},
+        unit: {type:String, required: false},
+        value: {type:Number, required:false}
+      }
+    },
+    soil_ch8: {
+      soilmoisture: {
+        time: {type:Number, required:false},
+        unit: {type:String, required: false},
+        value: {type:Number, required:false}
+      }
+    },
+    soilmoisture_sensor_ch4: {
+      time: {type:Number, required:false},
+      unit: {type:String, required: false},
+      value: {type:Number, required:false}
+    },
+    soilmoisture_sensor_ch5: {
+      time: {type:Number, required:false},
+      unit: {type:String, required: false},
+      value: {type:Number, required:false}
+    },
+    soilmoisture_sensor_ch6: {
+      time: {type:Number, required:false},
+      unit: {type:String, required: false},
+      value: {type:Number, required:false}
+    },
+    soilmoisture_sensor_ch7: {
+      time: {type:Number, required:false},
+      unit: {type:String, required: false},
+      value: {type:Number, required:false}
+    },
+    soilmoisture_sensor_ch8: {
+      time: {type:Number, required:false},
+      unit: {type:String, required: false},
+      value: {type:Number, required:false}
+    }
+  });
 
-interface SensorStats {
-    pm2_5: number;
-    pm2_5_10minute: number;
-    pm2_5_30minute: number;
-    pm2_5_60minute: number;
-    pm2_5_6hour: number;
-    pm2_5_24hour: number;
-    pm2_5_1week: number;
-    time_stamp: number;
-}
+  export const EcowittDataModel = BaseSoilModel.discriminator('ecowitt_data', EcowittDataSchema);
+
+  export interface EcowittData extends BaseSoilData {
+      
+  }
+
+  interface SensorStats {
+      pm2_5: number;
+      pm2_5_10minute: number;
+      pm2_5_30minute: number;
+      pm2_5_60minute: number;
+      pm2_5_6hour: number;
+      pm2_5_24hour: number;
+      pm2_5_1week: number;
+      time_stamp: number;
+  }
